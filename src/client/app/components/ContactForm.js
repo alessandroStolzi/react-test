82ac68c499d561e96f8da9308c3af943abb1124a
alessandro stolzi <piuveloce@hotmail.com>
2018-01-20 16:10:35 +0100

    preparazione async for saga

--- src/client/app/components/ContactForm.js
+++ src/client/app/components/ContactForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 import showResults from '../showResults';
+// import { setName } from '../actions/renameActions';
 import inputText from '../common-components/inputText';
 import Submit from '../common-components/Submit';
 
@@ -10,17 +11,20 @@ const validate = (values) => {
   if (!values.firstName) {
     errors.firstName = 'Required';
   }
-  if (!values.sec) {
-    errors.sec = 'Required';
-  }
+  // if (!values.sec) {
+  //   errors.sec = 'Required';
+  // }
   return errors;
 };
+// function submitA(values, dispatch) {
+//   return dispatch(setName(values.firstName));
+// }
 const ContactForm = (props) => {
   const { handleSubmit, submitting } = props;
   return (
     <form onSubmit={handleSubmit(showResults)}>
       <Field name="firstName" label="fefwefwf" component={inputText} type="text" />
-      <Field name="sec" label="sec" component={inputText} type="text" />
+      {/* <Field name="sec" label="sec" component={inputText} type="text" /> */}
       <Submit disabled={submitting || !props.valid} />
     </form>
   );
--- src/client/app/showResults.js
+++ src/client/app/showResults.js
@@ -1,10 +1,26 @@
-const showResults = values =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      // simulate server latency
-      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
-      resolve();
-    }, 500);
-  });
 
-export default showResults;
+// import { connect } from 'react-redux';
+import { setName } from './actions/renameActions';
+
+// const showResults = values => setName(values.firstName);
+export default function showResults(values, dispatch) {
+  //   new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     // simulate server latency
+  //     window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+
+  //     resolve({ uno: values.firstName, due: values.sec });
+  //   }, 500);
+  // });
+  return dispatch(setName(`bar ${values.firstName} baz`));
+}
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     setName: value => dispatch(setName(value))
+//   };
+// }
+
+// export default connect(mapDispatchToProps)(showResults);
+
+// export default showResults;
